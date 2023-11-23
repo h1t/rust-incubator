bbff336e1b99a2aa82be54743e0a01ad214f8c54
shem <openvg@mail.ru>
2023-11-23 17:46:06 +0200

    add use of Typestate pattern

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,17 +1,44 @@
+use std::marker::PhantomData;
+
 #[derive(Copy, Clone, Default)]
-struct Point {
+pub struct Point {
     x: i32,
     y: i32,
 }
 
 #[derive(Clone)]
-struct Polyline {
+enum EmptyPolylineState {}
+
+#[derive(Clone)]
+enum NonEmptyPolylineState {}
+
+pub trait PolylineState {}
+
+impl PolylineState for EmptyPolylineState {}
+impl PolylineState for NonEmptyPolylineState {}
+
+#[derive(Clone)]
+pub struct Polyline<S: PolylineState> {
     points: Vec<Point>,
+    _state: PhantomData<S>,
 }
 
-impl Polyline {
-    fn new(points: Vec<Point>) -> Option<Self> {
-        (!points.is_empty()).then_some(Self { points })
+impl Polyline<EmptyPolylineState> {
+    pub fn with_point(p: Point) -> Polyline<NonEmptyPolylineState> {
+        Polyline {
+            points: vec![p],
+            _state: PhantomData,
+        }
+    }
+}
+
+impl Polyline<NonEmptyPolylineState> {
+    pub fn add_point(&mut self, p: Point) {
+        self.points.push(p);
+    }
+
+    pub fn add_points<I: IntoIterator<Item = Point>>(&mut self, iter: I) {
+        self.points.extend(iter.into_iter());
     }
 }
 
@@ -20,6 +47,9 @@ fn main() {
     let p2 = p1.clone();
     let p3 = p1;
     let p4: Point = Default::default();
+
+    assert_eq!(p1.x, 1);
+    assert_eq!(p1.y, 2);
 
     assert_eq!(p2.x, 1);
     assert_eq!(p2.y, 2);
@@ -30,9 +60,12 @@ fn main() {
     assert_eq!(p4.x, 0);
     assert_eq!(p4.y, 0);
 
-    if let Some(poly1) = Polyline::new(vec![p1, p2, p3]) {
-        let poly2 = poly1.clone();
+    let mut poly = Polyline::with_point(p1);
+    poly.add_points(vec![p2, p3]);
+    poly.add_point(p4);
 
-        assert_eq!(poly2.points.len(), 3);
-    }
+    assert_eq!(poly.points.len(), 4);
+
+    let poly1 = poly.clone();
+    assert_eq!(poly1.points.len(), 4);
 }
