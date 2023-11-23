afea1dd940b7f19392758040c5e021916cb3c94b
shem <openvg@mail.ru>
2023-11-23 09:30:22 +0200

    1_1_default_clone_copy task complete

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,38 @@
+#[derive(Copy, Clone, Default)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone)]
+struct Polyline {
+    points: Vec<Point>,
+}
+
+impl Polyline {
+    fn new(points: Vec<Point>) -> Option<Self> {
+        (!points.is_empty()).then_some(Self { points })
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = p1.clone();
+    let p3 = p1;
+    let p4: Point = Default::default();
+
+    assert_eq!(p2.x, 1);
+    assert_eq!(p2.y, 2);
+
+    assert_eq!(p3.x, 1);
+    assert_eq!(p3.y, 2);
+
+    assert_eq!(p4.x, 0);
+    assert_eq!(p4.y, 0);
+
+    if let Some(poly1) = Polyline::new(vec![p1, p2, p3]) {
+        let poly2 = poly1.clone();
+
+        assert_eq!(poly2.points.len(), 3);
+    }
 }
