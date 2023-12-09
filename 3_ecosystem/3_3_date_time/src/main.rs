2785bfdbab4b9c9294695a3214bdea1a1ff02c18
shem <openvg@mail.ru>
2023-12-09 10:19:08 +0200

    add implementation of 3_3_date_time task

--- 3_ecosystem/3_3_date_time/src/main.rs
+++ 3_ecosystem/3_3_date_time/src/main.rs
@@ -14,7 +14,7 @@ struct User {
 impl User {
     fn with_birthdate(year: i32, month: u32, day: u32) -> Self {
         let month = Month::try_from(month as u8).expect("bad month value");
-        let birthdate = Date::from_calendar_date(year, month, day as u8).expect("bad date format");
+        let birthdate = Date::from_calendar_date(year, month, day as u8).expect("bad date params");
         Self { birthdate }
     }
 
@@ -22,16 +22,13 @@ impl User {
     fn age(&self) -> u16 {
         let format = format_description!("[year]-[month]-[day]");
         let now = Date::parse(NOW, &format).expect("can't parse NOW date");
-        let now_month = u8::from(now.month());
-        let now_day = now.day();
-        let user_month = u8::from(self.birthdate.month());
-        let user_day = self.birthdate.day();
         let mut age = now.year() - self.birthdate.year();
 
-        //NOTE: before birthday in this year
-        if user_month <= now_month && user_day <= now_day {
+        //NOTE: before birth day in the current year
+        if self.birthdate.ordinal() < now.ordinal() {
             age -= 1;
         }
+
         age.try_into().unwrap_or(0)
     }
 
@@ -49,6 +46,7 @@ mod age_spec {
     fn counts_age() {
         for ((y, m, d), expected) in [
             ((1990, 6, 4), 28),
+            ((1990, 6, 26), 29),
             ((1990, 7, 4), 29),
             ((0, 1, 1), 2018),
             ((1970, 1, 1), 48),
