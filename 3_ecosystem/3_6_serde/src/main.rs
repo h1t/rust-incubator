c585ec645d339ef64f172939c5a1c54b4de29b65
shem <openvg@mail.ru>
2023-12-10 19:01:14 +0200

    add printing out of TOML and YAML data of Request

--- 3_ecosystem/3_6_serde/src/main.rs
+++ 3_ecosystem/3_6_serde/src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::error::Error;
+use step_3_6::Request;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let json_data = include_str!("../request.json");
+    let request: Request = serde_json::from_str(json_data)?;
+    let yaml_data = serde_yaml::to_string(&request)?;
+    let toml_data = toml::to_string(&request)?;
+
+    println!("YAML:\n{yaml_data:?}");
+    println!();
+    println!("TOML:\n{toml_data:?}");
+
+    Ok(())
+}
