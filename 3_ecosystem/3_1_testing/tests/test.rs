801b555de7a6e0a64af8fe47626b53683fc23b15
shem <openvg@mail.ru>
2023-12-22 10:14:00 +0200

    add use of proptest crate for tests

--- 3_ecosystem/3_1_testing/tests/test.rs
+++ 3_ecosystem/3_1_testing/tests/test.rs
@@ -1,10 +1,37 @@
 #[cfg(test)]
 mod tests {
     use assert_cmd::Command;
+    use proptest::prelude::*;
     use std::cmp::Ordering;
     use std::fmt::Write;
 
     const BINARY_NAME: &str = "step_3_1";
+
+    proptest! {
+        #[test]
+        fn test_proptest(
+            secret in 0..100u32,
+            inputs in prop::collection::vec("[a-zA-Z0-9]*", 1..100)
+        ) {
+            check_proptest_values(secret, inputs);
+        }
+    }
+
+    fn check_proptest_values(secret: u32, mut inputs: Vec<String>) {
+        let secret_str = secret.to_string();
+        if let Some(pos) = inputs.iter().position(|s| secret_str == s.as_str()) {
+            let inputs = inputs
+                .iter()
+                .take(pos + 1)
+                .map(String::as_str)
+                .collect::<Vec<_>>();
+            check_success_output_with_inputs(secret, &inputs);
+        } else {
+            inputs.push(secret_str);
+            let inputs = inputs.iter().map(String::as_str).collect::<Vec<_>>();
+            check_success_output_with_inputs(secret, &inputs);
+        }
+    }
 
     #[test]
     fn test_success() {
@@ -23,17 +50,11 @@ mod tests {
 
     #[test]
     fn test_bad_input() {
-        let secret = 5;
-        let numbers = ["4", "test", "6", "5"];
-        let input = numbers.join("\n");
-        let numbers = numbers.iter().map(|s| s.parse().ok()).collect::<Vec<_>>();
-        let output = generate_output(secret, &numbers);
-
-        let mut cmd = Command::cargo_bin(BINARY_NAME).unwrap();
-        let assert = cmd.arg(secret.to_string());
-
-        assert.write_stdin(input);
-        assert.assert().success().stdout(output);
+        check_success_output_with_inputs(5, &["5"]);
+        check_success_output_with_inputs(5, &["", "5"]);
+        check_success_output_with_inputs(5, &["", "5"]);
+        check_success_output_with_inputs(5, &["a", "5"]);
+        check_success_output_with_inputs(5, &["a", "4", "b", "6", "c", "5"]);
     }
 
     fn check_failure_arg(arg: &str) {
@@ -54,11 +75,30 @@ mod tests {
         assert.assert().success().stdout(output);
     }
 
+    fn check_success_output_with_inputs(secret: u32, inputs: &[&str]) {
+        let mut cmd = Command::cargo_bin(BINARY_NAME).unwrap();
+        let assert = cmd.arg(secret.to_string());
+        let input = inputs.join("\n");
+        let output = generate_output_from_inputs(secret, inputs);
+
+        assert.write_stdin(input);
+        assert.assert().success().stdout(output);
+    }
+
     fn generate_input(numbers: &[u32]) -> String {
         numbers.iter().fold(String::new(), |mut output, n| {
             let _ = writeln!(output, "{n}");
             output
         })
+    }
+
+    fn generate_output_from_inputs(secret: u32, inputs: &[&str]) -> String {
+        let numbers = inputs
+            .iter()
+            .map(|s| s.parse::<u32>().ok())
+            .collect::<Vec<_>>();
+
+        generate_output(secret, &numbers)
     }
 
     fn generate_output(secret: u32, numbers: &[Option<u32>]) -> String {
